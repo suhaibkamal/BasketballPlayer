1ed2275c4a59012e81582a298e924d0fc24c6d42
suhaibkamal <suhaib49@gmail.com>
2025-02-05 00:33:58 +0300

    adding networking and api response model

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -40,6 +40,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.gson)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.logging)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ domain/src/main/java/com/petraride/domain/model/PlayerResponse.kt
@@ -0,0 +1,39 @@
+package com.petraride.domain.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PlayerResponse(
+    @SerializedName("data") val players: List<Player>,
+    @SerializedName("meta") val meta: Meta
+)
+
+data class Player(
+    @SerializedName("id") val id: Int,
+    @SerializedName("first_name") val firstName: String,
+    @SerializedName("last_name") val lastName: String,
+    @SerializedName("position") val position: String,
+    @SerializedName("height") val height: String,
+    @SerializedName("weight") val weight: String,
+    @SerializedName("jersey_number") val jerseyNumber: String,
+    @SerializedName("college") val college: String?, // Can be null
+    @SerializedName("country") val country: String,
+    @SerializedName("draft_year") val draftYear: Int?, // Can be null
+    @SerializedName("draft_round") val draftRound: Int?, // Can be null
+    @SerializedName("draft_number") val draftNumber: Int?, // Can be null
+    @SerializedName("team") val team: Team
+)
+
+data class Team(
+    @SerializedName("id") val id: Int,
+    @SerializedName("conference") val conference: String,
+    @SerializedName("division") val division: String,
+    @SerializedName("city") val city: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("full_name") val fullName: String,
+    @SerializedName("abbreviation") val abbreviation: String
+)
+
+data class Meta(
+    @SerializedName("next_cursor") val nextCursor: Int,
+    @SerializedName("per_page") val perPage: Int
+)
